ce786b5a0948bab457d104dceff31e35761e2c1c
root <root@ip-172-31-36-209.us-east-2.compute.internal>
2021-05-10 04:33:53 +0000

    fifth version

--- Simple.java
+++ Simple.java
@@ -1,5 +1,5 @@
 class Simple{
 	public static void main(String args[]){
-		System.out.println("Welcome to cloud devops new technology ");
+		System.out.println("Welcome to cloud devop new technology ");
 	}
 }
