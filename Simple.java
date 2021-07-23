d7f02f6ef63561b7f6a88c441828d6df76bc8c3c
root <root@ip-172-31-0-118.ec2.internal>
2021-07-23 08:03:19 +0000

    f7

--- Simple.java
+++ Simple.java
@@ -1,5 +1,5 @@
 class Simple{
 	public static void main(String args[]){
-		System.out.println("Welcome to cloud devop new technologyfifth ");
+		System.out.println("Welcome to cloud devop new technologseventh ");
 	}
 }
